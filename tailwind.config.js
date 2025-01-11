74e5af57abe8fb709a40243b54a48bc699b2fd17
gunce <guncebektas@gmail.com>
2025-01-11 09:11:09 +0300

    add: branding color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ module.exports = {
       pink: colors.fuchsia,
     },
     extend: {
+      colors: {
+        primary: {
+          '50': '#ffb3b5',
+          '100': '#ffb385',
+          '200': '#ff8083',
+          '300': '#ff4d51',
+          '400': '#ff1a1f',
+          '500': '#fb0010',
+          '600': '#cb0010',
+          '700': '#b3000e',
+          '800': '#80000a',
+          '900': '#4d0006',
+          '950': '#0d0001',
+        }
+      },
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
       },
