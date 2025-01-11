21926d700fa9ef2648f35ec7df6287f29fd4eaca
gunce <guncebektas@gmail.com>
2025-01-11 15:42:44 +0300

    vscode updates

--- client/ui/App.jsx
+++ client/ui/App.jsx
@@ -45,6 +45,7 @@ const customTheme = {
     color: {
       default: "py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white",
       primary: "w-full flex justify-center py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white !bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+      secondary: "w-full flex justify-center py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white !bg-secondary-600 hover:bg-secondary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
     },
   },
 };
