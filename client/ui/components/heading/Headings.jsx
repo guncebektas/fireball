5e8b45847081efffc54b92620c8c54ac601b6bbb
gunce <guncebektas@gmail.com>
2025-01-11 18:45:19 +0300

    add: translateWithSymbol

--- client/ui/components/heading/Headings.jsx
+++ client/ui/components/heading/Headings.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import {useTranslator} from "../../providers/i18n";
 import {BackButton} from "../buttons/BackButton";
 
+const translateWithSymbol = (text, t) => {
+  const lastChar = text.slice(-1);
+  const hasSymbol = /[!?:]/.test(lastChar);
+  const textContent = hasSymbol ? text.slice(0, -1) : text;
+  return t(textContent) + (hasSymbol ? lastChar : '');
+};
+
 export const H2 = ({text, showBackButton = false, ...props}) => {
   const t = useTranslator();
-
   return (
     <>
       <h2 className="m-title text-3xl mb-1" {...props}>
-        {showBackButton ? <BackButton /> : ''} {t(text)}
+        {showBackButton ? <BackButton /> : ''} {translateWithSymbol(text, t)}
       </h2>
     </>
   )
@@ -16,15 +22,15 @@ export const H2 = ({text, showBackButton = false, ...props}) => {
 
 export const H3 = ({text, ...props}) => {
   const t = useTranslator();
-  return (<h3 className="m-title text-2xl mb-3" {...props}>{t(text)}</h3>);
+  return (<h3 className="m-title text-2xl mb-3" {...props}>{translateWithSymbol(text, t)}</h3>);
 };
 
 export const H4 = ({text, ...props}) => {
   const t = useTranslator();
-  return (<h4 className="m-title text-xl mb-3" {...props}>{t(text)}</h4>);
+  return (<h4 className="m-title text-xl mb-3" {...props}>{translateWithSymbol(text, t)}</h4>);
 };
 
 export const H5 = ({text, ...props}) => {
   const t = useTranslator();
-  return (<h5 className="m-title text-lg" {...props}>{t(text)}</h5>);
+  return (<h5 className="m-title text-lg" {...props}>{translateWithSymbol(text, t)}</h5>);
 };
