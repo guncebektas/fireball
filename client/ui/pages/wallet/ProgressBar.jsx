c8e332671db293110cb209a966d6ab3584ef495a
gunce <guncebektas@gmail.com>
2025-01-11 11:21:53 +0300

    add: coinName to settings

--- client/ui/pages/wallet/ProgressBar.jsx
+++ client/ui/pages/wallet/ProgressBar.jsx
@@ -1,7 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Modal} from 'flowbite-react';
-import {H5} from "../../components/heading/Headings";
-import {Faqs} from "../help/Faqs";
+import React, {useEffect} from 'react';
 import {useTranslator} from "../../providers/i18n";
 import {useStampCountStore} from "../../stores/useStampCountStore";
 import {CircularProgressBar} from "../../components/progressBar/CircularProgressBar";
@@ -9,14 +6,12 @@ import {CircularProgressBar} from "../../components/progressBar/CircularProgress
 const ProgressBar = () => {
   const t = useTranslator();
 
-  const {logo} = Meteor.settings.public.app;
+  const {logo, coinName} = Meteor.settings.public.app;
 
-  const {stampCount , targetCount} = useStampCountStore();
+  const {stampCount, targetCount} = useStampCountStore();
 
   let progressPercentage = 0
   let reward = 0;
-
-  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     progressPercentage = (stampCount % targetCount / targetCount) * 100;
@@ -30,44 +25,29 @@ const ProgressBar = () => {
   return (
     <div className="mb-3">
       <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div className="flex w-1/3 items-center justify-center ml-2">
+          <div className="items-center justify-center">
+            <p className="m-text">{coinName}</p>
+            <h5 className="m-title">{reward}</h5>
+          </div>
+        </div>
+
         <CircularProgressBar
           progress={progressPercentage}
           size={100}
           strokeWidth={8}
           icon={logo}
         />
-      </div>
-      <p className="m-text text-center font-bold" style={{'margin-top': '-15px'}}>{stampCount % targetCount}/{targetCount}</p>
 
-      <div className="flex items-center space-x-4">
-        <H5 text={t(`Earn one coffee with {$target} stars`, {target: targetCount})}/>
-      </div>
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center ml-2">
-          <div className="w-10 h-10 flex items-center justify-center rounded-full bg-green-700 mr-1">
-            <span className="text-white lg font-bold">{reward}</span>
+        <div className="flex w-1/3 items-center justify-center ml-2">
+          <div className="items-center justify-center">
+            <p className="m-text">{t('Free')}</p>
+            <h5 className="m-title">{reward}</h5>
           </div>
-          <span className="text-green-700 dark:text-white font-bold text-lg ml-1">{t('Free')}</span>
         </div>
-        <button
-          className="text-lg text-blue-500 hover:underline"
-          onClick={() => setModalOpen(true)}
-        >
-          {t('Details')}
-        </button>
-
-        <Modal dismissible show={modalOpen} onClose={() => setModalOpen(false)}>
-          <Modal.Header>{t('FAQs')}</Modal.Header>
-          <Modal.Body>
-            <Faqs showTitle={false}/>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button color="default" onClick={() => setModalOpen(false)}>
-              {t('Close')}
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
+
+      <p className="m-text text-center font-bold" style={{'margin-top': '-15px'}}>{stampCount % targetCount}/{targetCount}</p>
     </div>
   );
 };
