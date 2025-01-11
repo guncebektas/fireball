a95aac463fc9df72e448675a4783c556fa813225
gunce <guncebektas@gmail.com>
2025-01-11 09:15:38 +0300

    add: userProfiles.updateBillingDetails

--- imports/modules/app/user/userProfiles/enums/method.js
+++ imports/modules/app/user/userProfiles/enums/method.js
@@ -1,6 +1,7 @@
 export const USER_PROFILES_METHOD = {
   UPDATE_DETAILS: 'userProfiles.updateDetails',
   UPDATE_PREFERENCES: 'userProfiles.updatePreferences',
+  UPDATE_BILLING_DETAILS: 'userProfiles.updateBillingDetails',
   SAVE_OTP: 'userProfiles.saveOtp',
   SAVE_PROFILE_PICTURE: 'userProfiles.saveProfilePicture'
 }
