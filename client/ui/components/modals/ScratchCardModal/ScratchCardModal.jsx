0a1e250f2247ee8107841e9331e3637928b6a0ff
gunce <guncebektas@gmail.com>
2025-03-15 10:51:22 +0300

    add: scratch card modal

--- client/ui/components/modals/ScratchCardModal/ScratchCardModal.jsx
+++ client/ui/components/modals/ScratchCardModal/ScratchCardModal.jsx
@@ -4,44 +4,80 @@ import {useTranslator} from "../../../providers/i18n";
 import {useScratchCardStore} from "../../../stores/useScratchCardStore";
 import {useStampCountStore} from "../../../stores/useStampCountStore";
 import {useConfettiStore} from "../../../stores/useConfettiStore";
-import {getWeightedReward, generateOtherNumbers, shuffleArray} from "./rewardUtils";
+import {generateOtherNumbers, getWeightedReward, shuffleArray} from "./rewardUtils";
 import ScratchCardContent from "./ScratchCardContent";
 import GameOverMessage from "./GameOverMessage";
+import {useWalletData} from "../../../hooks/useWalletData";
+import {userWalletMethod} from "../../../../../imports/modules/app/user/userWallet/userWalletMethod";
+import {userProfileRepository} from "../../../../../imports/modules/app/user/userProfiles/userProfileRepository";
+import {DateUtils} from "../../../../../imports/modules/shared/utils/DateUtils";
+import {useTracker} from "meteor/react-meteor-data";
+import {Meteor} from "meteor/meteor";
+import {USER_PROFILE_PUBLICATION} from "../../../../../imports/modules/app/user/userProfiles/enums/publication";
 
 const ScratchCardModal = () => {
   const t = useTranslator();
 
-  const isQRCodeModalOpen = useScratchCardStore((state) => state.isScratchCardModalOpen);
-  const closeQRCodeModal = useScratchCardStore((state) => state.closeScratchCardModal);
+  const isScratchCardModalOpen = useScratchCardStore((state) => state.isScratchCardModalOpen);
+  const closeScratchCardModal = useScratchCardStore((state) => state.closeScratchCardModal);
 
   const [canPlay, setCanPlay] = useState(true);
   const [gameOverMessage, setGameOverMessage] = useState('');
+  const [gameOverReward, setGameOverReward] = useState(0);
+  const [gameOverNextChanceAt, setGameOverNextChanceAt] = useState();
 
-  const {stampCount= 0, increaseStampCount} = useStampCountStore();
+  const {increaseStampCount} = useStampCountStore();
 
   const openConfetti = useConfettiStore((state) => state.openConfetti);
-  const closeConfetti = useConfettiStore((state) => state.closeConfetti);
 
-  const [reward, setReward] = useState(getWeightedReward());
+  const [weightedReward] = useState(getWeightedReward());
 
-  const otherNumbers = generateOtherNumbers(reward);
-  const allNumbers = [reward, reward, reward, otherNumbers[0], otherNumbers[0], otherNumbers[1]];
+  const otherNumbers = generateOtherNumbers(weightedReward);
+  const allNumbers = [weightedReward, weightedReward, weightedReward, otherNumbers[0], otherNumbers[0], otherNumbers[1]];
   const shuffledNumbers = shuffleArray([...allNumbers]);
 
   const handleComplete = async () => {
-    if (reward === 0) {
-      setGameOverMessage('No luck. Try again next week')
+    if (weightedReward === 0) {
+      setGameOverMessage('No luck. Try again next week!');
+      setGameOverReward(0)
     } else {
-      await increaseStampCount(reward);
-      openConfetti();
-      setGameOverMessage('Great. Try your luck again after a week');
+      await userWalletMethod.increaseCustomerStamp({amount: weightedReward})
+        .then(async response => {
+          openConfetti();
+          setGameOverMessage('Great. Try your luck again after a week!');
+          setGameOverReward(weightedReward);
+
+          await increaseStampCount(weightedReward);
+        })
+        .catch(error => {
+          console.error(error);
+        })
     }
 
     setCanPlay(false);
   };
 
+  let user;
+  useTracker(() => {
+    const subscription = Meteor.subscribe(USER_PROFILE_PUBLICATION.ME);
+    if (subscription.ready()) {
+      user = userProfileRepository.findOne({_id: Meteor.userId()}) || {};
+      if (DateUtils.IsInLastWeek(user?.scratchedAt)) {
+        setCanPlay(false);
+        setGameOverNextChanceAt(DateUtils.AddSevenDays(user?.scratchedAt).toLocaleDateString());
+      }
+    }
+  }, [user]);
+
+  const {refreshWalletData} = useWalletData();
+
+  const handleClose = () => {
+    closeScratchCardModal();
+    refreshWalletData();
+  };
+
   return (
-    <Modal dismissible show={isQRCodeModalOpen} onClose={closeQRCodeModal} size="md">
+    <Modal dismissible show={isScratchCardModalOpen} onClose={handleClose} size="md">
       <Modal.Header>
         {t('Scratch to win')}
       </Modal.Header>
@@ -49,7 +85,7 @@ const ScratchCardModal = () => {
         {canPlay ? (
           <ScratchCardContent shuffledNumbers={shuffledNumbers} handleComplete={handleComplete}/>
         ) : (
-          <GameOverMessage message={gameOverMessage || 'You can try your luck once in every week'}/>
+          <GameOverMessage message={gameOverMessage || 'You can try your luck once in every week'} reward={gameOverReward} nextChanceAt={gameOverNextChanceAt}/>
         )}
       </Modal.Body>
     </Modal>
