5d127a9e50b9dda8efa13f6c4d07f2d276cf6714
gunce <guncebektas@gmail.com>
2025-05-14 20:30:04 +0300

    feat: disable jam:pub-sub

--- imports/modules/app/faqs/server/publications.js
+++ imports/modules/app/faqs/server/publications.js
@@ -3,7 +3,7 @@ import {FAQS_PUBLICATION} from "../enums/publication.js";
 import {faqRepository} from "../faqRepository";
 import {createProjection} from "../../../shared/functions/createProjection";
 
-Meteor.publish.stream(FAQS_PUBLICATION.ONE, function (_id) {
+Meteor.publish(FAQS_PUBLICATION.ONE, function (_id) {
   if (!this.userId) {
     return this.ready();
   }
@@ -11,7 +11,7 @@ Meteor.publish.stream(FAQS_PUBLICATION.ONE, function (_id) {
   return faqRepository.find({_id});
 });
 
-Meteor.publish.stream(FAQS_PUBLICATION.ALL, function (columns) {
+Meteor.publish(FAQS_PUBLICATION.ALL, function (columns) {
   if (!this.userId) {
     return this.ready();
   }
@@ -21,7 +21,7 @@ Meteor.publish.stream(FAQS_PUBLICATION.ALL, function (columns) {
   return faqRepository.find({}, {...projection, ...{ sort: { order: 1 } }});
 });
 
-Meteor.publish.once(FAQS_PUBLICATION.ALL_ONCE, function (columns) {
+Meteor.publish(FAQS_PUBLICATION.ALL_ONCE, function (columns) {
   if (!this.userId) {
     return this.ready();
   }
--- imports/modules/app/notifications/server/publish.js
+++ imports/modules/app/notifications/server/publish.js
@@ -2,7 +2,7 @@ import {Meteor} from "meteor/meteor";
 import {NOTIFICATIONS_PUBLICATION} from "../enums/publication";
 import {notificationRepository} from "../notificationRepository";
 
-Meteor.publish.stream(NOTIFICATIONS_PUBLICATION.ALL, function () {
+Meteor.publish(NOTIFICATIONS_PUBLICATION.ALL, function () {
   return notificationRepository.find({
     organizationId: Meteor.settings.public.app._id
   });
