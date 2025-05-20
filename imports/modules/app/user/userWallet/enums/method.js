62e581938c834bfbcd67ee9a18c43444a7a16e85
gunce <guncebektas@gmail.com>
2025-05-20 22:12:20 +0300

    add: POST_CUSTOMER

--- imports/modules/app/user/userWallet/enums/method.js
+++ imports/modules/app/user/userWallet/enums/method.js
@@ -1,4 +1,5 @@
 export const USER_WALLET_METHOD = {
   GET_CUSTOMER: 'userWallet.getCustomer',
+  POST_CUSTOMER: 'userWallet.postCustomer',
   INCREASE_STAMP_COUNT: 'userWallet.increaseStampCount'
 }
