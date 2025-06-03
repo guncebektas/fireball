7b14804264c52f861e3250bb279592fe9a8a32f0
gunce <guncebektas@gmail.com>
2025-06-03 23:09:15 +0300

    add: GET_PRODUCTS_FILTERED_BY_IMAGES

--- imports/modules/app/stores/enums/method.stores.js
+++ imports/modules/app/stores/enums/method.stores.js
@@ -1,4 +1,5 @@
 export const STORE_METHOD = {
   GET_PRODUCT_CATEGORIES: 'stores.getProductCategories',
-  GET_PRODUCTS: 'stores.getProducts'
+  GET_PRODUCTS: 'stores.getProducts',
+  GET_PRODUCTS_FILTERED_BY_IMAGES: 'stores.getProductsFilteredByImages',
 }
