06938cb0710cdafaf7c2d890e40b5c1dfb8ca7e9
gunce <guncebektas@gmail.com>
2025-07-02 11:29:38 +0300

    feat: avatar image

--- client/ui/components/profileAvatar/profileAvatar.jsx
+++ client/ui/components/profileAvatar/profileAvatar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import {Avatar} from "flowbite-react";
 
 export const ProfileAvatar = ({path}) => {
-  if (!path) {
-    return ''
+  if (path) {
+    return (<Avatar img={`https://ritapos-files.s3.eu-central-1.amazonaws.com/${path}`} alt="Avatar" className="mr-3" rounded/>);
   }
 
-  return (<Avatar img={`https://ritapos-files.s3.eu-central-1.amazonaws.com/${path}`} alt="Avatar" className="mr-3" rounded/>);
+  return '';
 };
