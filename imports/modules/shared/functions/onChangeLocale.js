60896d7f2ae3e5e8f0c05cd7406e0b923cb14b38
gunce <guncebektas@gmail.com>
2025-09-15 08:57:09 +0300

    add: de-DE

--- imports/modules/shared/functions/onChangeLocale.js
+++ imports/modules/shared/functions/onChangeLocale.js
@@ -3,6 +3,7 @@ import i18n from "meteor/universe:i18n";
 export const onChangeLocale = async locale => {
   /* eslint-disable no-unreachable */
   if (false) {
+    import '../../../../translations/de-DE.i18n.json';
     import '../../../../translations/en-US.i18n.json';
     import '../../../../translations/tr-TR.i18n.json';
   }
