8f12182592471b577c3c7182527a879bb22374f4
gunce <guncebektas@gmail.com>
2025-09-15 08:56:56 +0300

    fix: dot notation

--- client/ui/pages/auth/legal/TermsAndConditions.jsx
+++ client/ui/pages/auth/legal/TermsAndConditions.jsx
@@ -15,7 +15,7 @@ export const TermsAndConditions = ({fullPage}) => {
       )}
       <div className="text-lg text-gray-500 space-y-6 mt-3 mb-8">
         <H3 text={t('terms.title.1')}/>
-        <p>{t('terms.content.1', {
+        <p>{t('terms.content.1-1', {
           appName: name,
           companyName: company.name,
           companyAddress: company.address,
@@ -26,49 +26,49 @@ export const TermsAndConditions = ({fullPage}) => {
           companyTaxId: company.taxId,
           companyWebsite: company.website
         })}</p>
-        <p>{t('terms.content.2')}</p>
-        <p>{t('terms.content.3')}</p>
+        <p>{t('terms.content.1-2')}</p>
+        <p>{t('terms.content.1-3')}</p>
 
         <H3 text={t('terms.title.2')}/>
-        <p>{t('terms.content.4', {appName: name})}</p>
+        <p>{t('terms.content.2-1', {appName: name})}</p>
 
         <H3 text={t('terms.title.3')}/>
-        <p>{t('terms.content.3.1')}</p>
-        <p>{t('terms.content.3.2')}</p>
-        <p>{t('terms.content.3.3')}</p>
-        <p>{t('terms.content.3.4')}</p>
+        <p>{t('terms.content.3-1')}</p>
+        <p>{t('terms.content.3-2')}</p>
+        <p>{t('terms.content.3-3')}</p>
+        <p>{t('terms.content.3-4')}</p>
 
         <H3 text={t('terms.title.4')}/>
-        <p>{t('terms.content.4.1')}</p>
-        <p>{t('terms.content.4.2')}</p>
-        <p>{t('terms.content.4.3')}</p>
-        <p>{t('terms.content.4.4')}</p>
-        <p>{t('terms.content.4.5')}</p>
-        <p>{t('terms.content.4.6')}</p>
-        <p>{t('terms.content.4.7')}</p>
+        <p>{t('terms.content.4-1')}</p>
+        <p>{t('terms.content.4-2')}</p>
+        <p>{t('terms.content.4-3')}</p>
+        <p>{t('terms.content.4-4')}</p>
+        <p>{t('terms.content.4-5')}</p>
+        <p>{t('terms.content.4-6')}</p>
+        <p>{t('terms.content.4-7')}</p>
 
         <H3 text={t('terms.title.5')}/>
-        <p>{t('terms.content.5.1')}</p>
-        <p>{t('terms.content.5.2')}</p>
-        <p>{t('terms.content.5.3')}</p>
-        <p>{t('terms.content.5.4')}</p>
-        <p>{t('terms.content.5.5')}</p>
-        <p>{t('terms.content.5.6')}</p>
-        <p>{t('terms.content.5.7')}</p>
+        <p>{t('terms.content.5-1')}</p>
+        <p>{t('terms.content.5-2')}</p>
+        <p>{t('terms.content.5-3')}</p>
+        <p>{t('terms.content.5-4')}</p>
+        <p>{t('terms.content.5-5')}</p>
+        <p>{t('terms.content.5-6')}</p>
+        <p>{t('terms.content.5-7')}</p>
 
         <H3 text={t('terms.title.6')}/>
-        <p>{t('terms.content.6.1')}</p>
-        <p>{t('terms.content.6.2')}</p>
+        <p>{t('terms.content.6-1')}</p>
+        <p>{t('terms.content.6-2')}</p>
 
         <H3 text={t('terms.title.7')}/>
-        <p>{t('terms.content.7.1', {
+        <p>{t('terms.content.7-1', {
           companyMainEmail: company.email.main,
           companyPhone: company.phone
         })}</p>
 
         <H3 text={t('terms.title.8')}/>
-        <p>{t('terms.content.8.1')}</p>
-        <p>{t('terms.content.8.2')}</p>
+        <p>{t('terms.content.8-1')}</p>
+        <p>{t('terms.content.8-2')}</p>
       </div>
     </>
   );
